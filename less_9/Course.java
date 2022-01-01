38a933ca634349fe8308b68204e3916c3d2b253e
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2022-01-01 22:57:39 +0300

    Create Course.java

---
+++ less_9/Course.java
@@ -0,0 +1,22 @@
+package javacore.less_9;
+
+public class Course {
+    private String name;
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
